f37ed108122ada9d2847b835d8cb375c8f059f5f
Stev51 <87919966+Stev51@users.noreply.github.com>
2024-07-18 00:22:26 -0500

    arrows are cool now

--- scenes/arrow.gd
+++ scenes/arrow.gd
@@ -5,9 +5,19 @@ const SPEED = 600.0
 
 var shooter
 var velocity
+var flying = true
 
 func _ready():
 	velocity = Vector2(1, 0).rotated(rotation)
 
 func _physics_process(delta):
-	position += velocity * SPEED * delta
+	if flying:
+		position += velocity * SPEED * delta
+
+func _on_despawn_collision_body_entered(body):
+	if body.is_in_group("colliders"):
+		flying = false
+
+func _on_body_entered(body):
+	if flying:
+		pass #do dmg (this if statement disables non-flying arrows from damaging)
