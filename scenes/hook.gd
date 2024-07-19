6b9fb703b9e44b462bfb9f9fa83248e4e0c2a993
Stev51 <87919966+Stev51@users.noreply.github.com>
2024-07-19 13:33:39 -0500

    hooks finalized maybe for now

--- scenes/hook.gd
+++ scenes/hook.gd
@@ -33,8 +33,10 @@ func deactivate():
 	print("deactivated")
 
 func _on_body_entered(body):
-	if body.is_in_group("colliders"):
+	if body.is_in_group("walls"):
 		flying = false
+	elif body.is_in_group("doors"):
+		queue_free()
 
 func _on_area_exited(area):
 	if area.is_in_group("screen"):
