eccf6629118973b313cf5960ae24b3eeddbf76c3
Stev51 <87919966+Stev51@users.noreply.github.com>
2024-07-21 14:27:05 -0500

    switches mostly working

--- scripts/world.gd
+++ scripts/world.gd
@@ -40,15 +40,21 @@ func checkpoint(area):
 	$Player.respawn_pos = area.position
 
 func switch_process(switch, tile_pos):
+	if tile_pos == null:
+		print("WARNING: UNDEFINED SWITCH LINK")
+	$TileMap.set_cell(3, tile_pos, 5, Vector2i(0,1))
 	switch.queue_free()
 
 func door_process(door, tile_pos, new_tile):
+	if tile_pos == null or new_tile == null:
+		print("WARNING: UNDEFINED DOOR LINK")
+	$TileMap.set_cell(2, tile_pos, 4, new_tile)
 	door.queue_free()
 
 func incr_boss_switches():
 	boss_switches += 1
 	if boss_switches >= 4:
-		door_process($DoorCollisions/Doors3, null)
+		door_process($DoorCollisions/Doors3, Vector2i(24,-11), Vector2i(5,0))
 
 func _on_chasm_area_body_entered(body):
 	if body.is_in_group("player"):
@@ -82,33 +88,33 @@ func _on_checkpoint_4_body_entered(body):
 
 func _on_switch_1_area_entered(area):
 	if area.is_in_group("sword") and Global.attacking:
-		switch_process($Switches/Switch1, null)
+		switch_process($Switches/Switch1, Vector2i(8,6))
 		incr_boss_switches()
 
 func _on_switch_2_area_entered(area):
 	if area.is_in_group("sword") and Global.attacking:
-		switch_process($Switches/Switch2, null)
+		switch_process($Switches/Switch2, Vector2i(8,-18))
 		incr_boss_switches()
 
 func _on_switch_3_area_entered(area):
 	if area.is_in_group("sword") and Global.attacking:
-		switch_process($Switches/Switch3, null)
+		switch_process($Switches/Switch3, Vector2i(56,-18))
 		incr_boss_switches()
 
 func _on_switch_4_area_entered(area):
 	if area.is_in_group("sword") and Global.attacking:
-		switch_process($Switches/Switch4, null)
-		door_process($DoorCollisions/Doors4, null)
-		door_process($DoorCollisions/Doors5, null)
+		switch_process($Switches/Switch4, Vector2i(56,6))
+		door_process($DoorCollisions/Doors4, Vector2i(56,-1), Vector2i(5,4))
+		door_process($DoorCollisions/Doors5, Vector2i(56,1), Vector2i(5,0))
 		incr_boss_switches()
 
 func _on_switch_5_area_entered(area):
 	if area.is_in_group("sword") and Global.attacking:
-		switch_process($Switches/Switch5, null)
-		door_process($DoorCollisions/Doors2, null)
+		switch_process($Switches/Switch5, Vector2i(56,-6))
+		door_process($DoorCollisions/Doors2, Vector2i(8,-11), Vector2i(5,0))
 
 func _on_arrow_switch_1_area_entered(area):
 	if area.is_in_group("arrows"):
 		area.queue_free()
-		switch_process($Switches/ArrowSwitch1, null)
-		door_process($DoorCollisions/Doors7, null)
+		switch_process($Switches/ArrowSwitch1, Vector2i(40,1))
+		door_process($DoorCollisions/Doors7, Vector2i(47,6), Vector2i(8,1))
