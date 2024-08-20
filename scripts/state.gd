dfb48015cbeac474d4ca57d2c8c05253daee3ae8
Stev51 <87919966+Stev51@users.noreply.github.com>
2024-08-20 17:48:13 -0400

    last stuff done before break

---
+++ scripts/state.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name State
+
+var state_machine = null
+
+func handle_input(event: InputEvent) -> void:
+	pass
+
+func physics_update(delta: float) -> void:
+	pass
+
+func enter(msg := {}) -> void:
+	pass
+
+func exit() -> void:
+	pass
